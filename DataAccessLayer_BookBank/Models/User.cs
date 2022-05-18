1a9152d38ef0fe118cf10f787431eabe2e3c65fc
kher976 <sagarkher08@gmail.com>
2022-05-18 10:48:11 +0530

    sagar kher

--- DataAccessLayer_BookBank/Models/User.cs
+++ DataAccessLayer_BookBank/Models/User.cs
@@ -6,6 +6,7 @@ namespace DataAccessLayer_BookBank.Models
 {
     public class User
     {
+        //adding sagar
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string MobileNo { get; set; }
